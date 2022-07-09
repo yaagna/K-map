4f0a08bf0056e43a265a9cf2fcbf0261b3cb4434
Yaagna <yaagnamodi@gmail.com>
2022-07-09 22:37:55 +0530

    commit

--- K-map.py
+++ K-map.py
@@ -1,9 +1,12 @@
+
 #This program is used to minimize logical expression using Quine Mccluskey algorithm
+
 
 #multiply 2 minterms
 def minterm_multiply(x,y):
     res = []
     for i in x:
+
         if i+"'" in y or (len(i)==2 and i[0] in y):
             return []
         else:
