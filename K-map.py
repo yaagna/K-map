414840bc8c2f3da6d6af42aacd0f6cb0347c9a47
Yaagna <yaagnamodi@gmail.com>
2022-10-28 15:23:16 +0530

    commit

--- K-map.py
+++ K-map.py
@@ -2,18 +2,34 @@
 #This program is used to minimize logical expression using Quine Mccluskey algorithm
 
 
-#multiply 2 minterms
-def minterm_multiply(x,y):
-    res = []
-    for i in x:
+#for 2 input Kmap
+def Kmap2(mt, nip):
+    for i in range(50):
+        print("-", end="")
+    print()
+    prnt = input("Enter Variables eg. (A B):").split(" ")
+    for i in range(len(mt)):
+        mt[i] = '0b'+ bin(mt[i])[2:].lstrip('0')
+    op=''
+    ans=[[0,0],[0,0]]
+    ansmx = [[0,0],[0,0]]
+    flag = 0
+    temp = []
 
-        if i+"'" in y or (len(i)==2 and i[0] in y):
-            return []
-        else:
-            res.append(i)
-    for i in y:
-        if i not in res:
-            return []
-        else:
-            res.append(i)
-    return res
+    for i in range(2):
+        for j in range(2):
+            p = '0b'+ (bin(i)[2:]+bin(j)[2:]).lstrip('0')
+            if p in mt:
+                ans[i][j] = 1
+    
+    for i in range(50):
+        print("-", end="")
+    print()
+    print("K-map done")
+    if nip==1:
+        for i in range(2):
+            for j in range(2):
+                if ans[i][j] == 1:
+                    ansmx[i][j] = 0 
+            
+        
